ac6ea02e6e7f97f8a77edde9895c8ed56e85f006
Sreten <scvetojevic1402@gmail.com>
2016-11-07 20:49:44 -0500

    Create total_number_of_records_in_each_table.sql
    This short function shows counts of records in each table in a database. Counts are printed as text with comma separated thousands. Can be useful for monitoring volume of inserts in each table.

---
+++ total_number_of_records_in_each_table.sql
@@ -0,0 +1,9 @@
+create or replace function total_records_per_db() returns table (schemaname name,relname name, cnt TEXT)
+as $$
+begin
+	return query SELECT a.schemaname,a.relname,to_char(a.n_live_tup,'999G999G999G990D00') as cnt FROM pg_stat_user_tables a ORDER BY n_live_tup DESC;
+end;
+$$language plpgsql;
+
+--usage:
+--select (total_records_per_db()).schemaname,(total_records_per_db()).relname,(total_records_per_db()).cnt;
