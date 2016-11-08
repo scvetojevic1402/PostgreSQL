70ebbdafa427b36b9f365216559319b3d4718b3a
Sreten <scvetojevic1402@gmail.com>
2016-11-07 21:46:36 -0500

    Update total_number_of_records_in_each_table.sql

--- total_number_of_records_in_each_table.sql
+++ total_number_of_records_in_each_table.sql
@@ -1,9 +1,9 @@
-create or replace function total_records_per_db() returns table (schemaname name,relname name, cnt TEXT)
+create or replace function total_records_per_db() returns table (schemaname name,relname name, n TEXT)
 as $$
 begin
-	return query SELECT a.schemaname,a.relname,to_char(a.n_live_tup,'999G999G999G990D00') as cnt FROM pg_stat_user_tables a ORDER BY n_live_tup DESC;
+	return query SELECT a.schemaname,a.relname,to_char(a.n_live_tup,'999G999G999G990D00') as n FROM pg_stat_user_tables a ORDER BY n_live_tup DESC;
 end;
 $$language plpgsql;
 
 --usage:
---select (total_records_per_db()).schemaname,(total_records_per_db()).relname,(total_records_per_db()).cnt;
+--select (total_records_per_db()).schemaname,(total_records_per_db()).relname,(total_records_per_db()).n;
